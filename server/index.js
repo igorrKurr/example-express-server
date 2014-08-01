da2678d39b3ffd8a8564091b7c61a8d7241c462e
Igor Kur <kur91@mail.ru>
2014-08-02 01:27:19 +0300

    Init repo

---
+++ server/index.js
@@ -0,0 +1,32 @@
+var express = require('express'),
+    app = express();
+
+app.get('/data', function(req, res){
+  var data = {
+    "users": [
+      {
+        "id": 1,
+        "name": "Lev",
+        "surname": "Tolstoy",
+        "address": {
+          "country": "UKR",
+          "city": "Kiev"
+        }
+      },
+      {
+        "id": 2,
+        "name": "Lev",
+        "surname": "Prostoy",
+        "address": {
+          "country": "USA",
+          "city": "NYC"
+        }
+      }
+    ]
+  };
+  res.send(data);
+});
+
+var server = app.listen(3000, function() {
+    console.log('Listening on port %d', server.address().port);
+});
