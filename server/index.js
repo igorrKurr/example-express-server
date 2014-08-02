4a18d667060e6b784414d7ead8bc13fca5b3ed82
Igor Kur <kur91@mail.ru>
2014-08-02 14:48:07 +0300

    Add search stub

--- server/index.js
+++ server/index.js
@@ -1,32 +1,41 @@
 var express = require('express'),
-    cors = require('cors'),
+    cors = require('cors')
+    Chance = require('chance'),
+    chance = new Chance(),
     app = express();
 
 app.use(cors());
 
+var users = [];
+for (var i = 0; i < 20; i++) {
+  users.push({
+    "id": chance.guid(),
+    "name": chance.first(),
+    "surname": chance.last(),
+    "address": {
+      "street": chance.street({short_suffix: true}),
+      "city": chance.city()
+    }
+  });
+}
+
+console.log(users);
+
 app.get('/data', function(req, res){
-  var data = {
-    "users": [
-      {
-        "id": 1,
-        "name": "Lev",
-        "surname": "Tolstoy",
-        "address": {
-          "country": "UKR",
-          "city": "Kiev"
-        }
-      },
-      {
-        "id": 2,
-        "name": "Lev",
-        "surname": "Prostoy",
-        "address": {
-          "country": "USA",
-          "city": "NYC"
-        }
-      }
-    ]
-  };
+  var data = {"users": users};
+  res.send(data);
+});
+
+var filterQueryByName = function (query, array) {
+  var re = new RegExp(query,'i');
+
+    return array.filter(function(item){
+      return re.test(item.name);
+    });
+}
+
+app.get('/search', function(req, res){
+  data = {"users": filterQueryByName(req.query.search, users)};
   res.send(data);
 });
 
